0bab34c4d03927e95620b7d0d6c1449549f4fe45
Alexeyvolsky <aleksvolsky@gmail.com>
2022-12-02 08:41:14 +0300

    add

--- src/test/java/ShareLaneTests.java
+++ src/test/java/ShareLaneTests.java
@@ -9,46 +9,44 @@ import org.testng.annotations.Test;
 
 public class ShareLaneTests {
     WebDriver driver;
-//
-//    @AfterMethod(alwaysRun = true)
-//    public void closeDriver() {
-//        if (driver != null) {
-//            driver.quit();
-//        }
-//    }
 
-//    @Test
-//    public void pozitiveZipCodeTest() {
-//        WebDriverManager.chromedriver().setup();
-//        driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//        driver.get("https://www.sharelane.com/cgi-bin/register.py");
-//        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
-//        zipCodeInput.sendKeys("12345");
-//        WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
-//        continueButton.click();
-//        zipCodeInput = driver.findElement(By.name("zip_code"));
-//        Assert.assertEquals(zipCodeInput.isDisplayed(), false);
-//        WebElement firstNameInput = driver.findElement(By.name("first_name"));
-//        Assert.assertEquals(firstNameInput.isDisplayed(), true);
-//        Thread.sleep(5000);
-//        driver.quit();
-//    }
-//    @Test
-//    public void negativeZipCodeTest(){
-//        WebDriverManager.chromedriver().setup();
-//        driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//        driver.get("https://www.sharelane.com/cgi-bin/register.py");
-//        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
-//        zipCodeInput.sendKeys("1946");
-//        WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
-//        continueButton.click();
-//        zipCodeInput = driver.findElement(By.name("zip_code"));
-//        Assert.assertEquals(zipCodeInput.isDisplayed(), false);
-//        WebElement firstNameInput = driver.findElement(By.name("first_name"));
-//        Assert.assertEquals(firstNameInput.isDisplayed(), true);
-//    }
+    @AfterMethod(alwaysRun = true)
+    public void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void pozitiveZipCodeTest() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("12345");
+        WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
+        continueButton.click();
+        zipCodeInput = driver.findElement(By.name("zip_code"));
+        Assert.assertEquals(zipCodeInput.isDisplayed(), false);
+        WebElement firstNameInput = driver.findElement(By.name("first_name"));
+        Assert.assertEquals(firstNameInput.isDisplayed(), true);
+    }
+    @Test
+    public void negativeZipCodeTest(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("1946");
+        WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
+        continueButton.click();
+        zipCodeInput = driver.findElement(By.name("zip_code"));
+        Assert.assertEquals(zipCodeInput.isDisplayed(), false);
+        WebElement firstNameInput = driver.findElement(By.name("first_name"));
+        Assert.assertEquals(firstNameInput.isDisplayed(), true);
+    }
     @Test
     public void pozitiveSignUp(){
         WebDriverManager.chromedriver().setup();
@@ -74,8 +72,36 @@ public class ShareLaneTests {
         confirmPasswordInput.sendKeys("123456789");
         WebElement registerButton = driver.findElement(By.cssSelector("[value=Register]"));
         registerButton.click();
-        firstNameInput = driver.findElement(By.name("first_name"));
-        Assert.assertEquals(firstNameInput.isDisplayed(), false);
+        WebElement accountIsCreated = driver.findElement(By.cssSelector("[class=confirmation_message]"));
+        Assert.assertEquals(accountIsCreated.isDisplayed(),true);
+    }
+    @Test
+    public void negativeSignUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("12345");
+        WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
+        continueButton.click();
+        zipCodeInput = driver.findElement(By.name("zip_code"));
+        Assert.assertEquals(zipCodeInput.isDisplayed(), false);
+        WebElement firstNameInput = driver.findElement(By.name("first_name"));
+        Assert.assertEquals(firstNameInput.isDisplayed(), true);
+        firstNameInput.sendKeys("Alexey");
+        WebElement lastNameInput = driver.findElement(By.name("last_name"));
+        lastNameInput.sendKeys("Volskiy");
+        WebElement emailInput = driver.findElement(By.name("email"));
+        emailInput.sendKeys("volskiy.com");
+        WebElement passwordInput = driver.findElement(By.name("password1"));
+        passwordInput.sendKeys("123456789");
+        WebElement confirmPasswordInput = driver.findElement(By.name("password2"));
+        confirmPasswordInput.sendKeys("123456789");
+        WebElement registerButton = driver.findElement(By.cssSelector("[value=Register]"));
+        registerButton.click();
+        passwordInput = driver.findElement(By.name("password1"));
+        Assert.assertEquals(passwordInput.isDisplayed(),false);
         WebElement accountIsCreated = driver.findElement(By.cssSelector("[class=confirmation_message]"));
         Assert.assertEquals(accountIsCreated.isDisplayed(),true);
     }
